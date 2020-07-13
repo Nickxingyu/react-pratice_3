f5c730e6f19793279d5226ad7877ba7b33457800
Hero <nick19960723@gmail.com>
2020-07-13 11:36:56 +0800

    Updates

---
+++ precache-manifest.4e30418846c8a6a73ece5ff99271e0cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97aee725eb1e16db4bf191ebf3e5ee5",
+    "url": "/Nickxingyu/frontend-practice-07-09/index.html"
+  },
+  {
+    "revision": "f9c0c78ecc507c297830",
+    "url": "/Nickxingyu/frontend-practice-07-09/static/css/main.30bc5495.chunk.css"
+  },
+  {
+    "revision": "da0265629040046a163d",
+    "url": "/Nickxingyu/frontend-practice-07-09/static/js/2.d2560713.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nickxingyu/frontend-practice-07-09/static/js/2.d2560713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c0c78ecc507c297830",
+    "url": "/Nickxingyu/frontend-practice-07-09/static/js/main.12c2d0eb.chunk.js"
+  },
+  {
+    "revision": "723c9cfccb9daa4cf33d",
+    "url": "/Nickxingyu/frontend-practice-07-09/static/js/runtime-main.241d2d87.js"
+  }
+]);
